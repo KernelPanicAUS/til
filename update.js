8b44ecce9b1909fd0f870e3c48b140a6ca772d27
Thomas Khalil <thomas@khalilt.com>
2020-04-28 13:57:30 +0200

    prettier

--- update.js
+++ update.js
@@ -4,34 +4,32 @@ const nunjucks = require('nunjucks');
 const getDirectories = source =>
   readdirSync(source, { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
-    .filter(dirent => dirent.name.startsWith(".") == false)
-    .filter(dirent => dirent.name.startsWith("node_") == false)
-    .map(dirent => dirent.name)
+    .filter(dirent => dirent.name.startsWith('.') == false)
+    .filter(dirent => dirent.name.startsWith('node_') == false)
+    .map(dirent => dirent.name);
 
 const getDirContents = source =>
   readdirSync(source, { withFileTypes: true })
     .filter(fileent => fileent.isFile())
-    .map(fileent => fileent.name)
+    .map(fileent => fileent.name);
 
 const getTitleFromFile = source =>
-  readFileSync(source, "utf8")
-    .split("\n")
-    .filter(line => line.startsWith("#"))
-    .map(line => line.split("# ")[1])
+  readFileSync(source, 'utf8')
+    .split('\n')
+    .filter(line => line.startsWith('#'))
+    .map(line => line.split('# ')[1]);
 
-const structures = getDirectories(__dirname)
-  .map(topic => {
-    return {
-      name: topic,
-      articles: getDirContents(`${__dirname}/${topic}/`)
-        .map(article => {
-          return {
-            name: getTitleFromFile(`${__dirname}/${topic}/${article}`)[0],
-            file: article
-          }
-        })
-    }
-  })
+const structures = getDirectories(__dirname).map(topic => {
+  return {
+    name: topic,
+    articles: getDirContents(`${__dirname}/${topic}/`).map(article => {
+      return {
+        name: getTitleFromFile(`${__dirname}/${topic}/${article}`)[0],
+        file: article
+      };
+    })
+  };
+});
 
 const res = nunjucks.render('./README.md.tpl', { categories: structures });
 
